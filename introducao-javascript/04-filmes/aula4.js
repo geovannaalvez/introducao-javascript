1ec8cf01c524bffb1479c58316c9372a9a5e7455
giovannadossantosvieira <geovannawwalves@gmail.com>
2026-08-18 16:47:14 -0300

    introdução Javascript

---
+++ introducao-javascript/04-filmes/aula4.js
@@ -0,0 +1,7 @@
+const tituloFilme = "Aventura Espacial";
+const classificacaoMinima = 12;
+const idadeEspectador = 14;
+const podeAssistir = idadeEspectador >= classificacaoMinima;
+
+console.log(podeAssistir);
+document.getElementById("resultado").textContent = `"$(tituloFilme)"` + ` (${classificacaoMinima} anos)` + `- idade ${idadeEspectador}:` + `pode assistir? ${podeAssistir}`;
